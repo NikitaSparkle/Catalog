0ed755e1fd86a47fc2397290aeb99ecb8aeba393
Alexey <irresjeny@gmail.com>
2021-11-22 09:45:14 +0200

    v0.3

--- notdiensture_server/forms.py
+++ notdiensture_server/forms.py
@@ -7,13 +7,13 @@ class SingUpForm(forms.Form):
     email = forms.EmailField(label="Электронная почта", required=True)
     phone = forms.CharField(label="Номер телефона", required=True, max_length=32)
     country = forms.ModelChoiceField(label="Страна", queryset=Country.objects.filter(), required=True)
-    password = forms.CharField(label="Пароль", max_length=16, required=True)
-    password1 = forms.CharField(label="Повторите пароль", max_length=16, required=True)
+    password = forms.CharField(label="Пароль", max_length=16, required=True, widget=forms.PasswordInput)
+    password1 = forms.CharField(label="Повторите пароль", max_length=16, required=True, widget=forms.PasswordInput)
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="Email", required=True)
-    password = forms.CharField(label="Пароль", max_length=16, required=True)
+    password = forms.CharField(label="Пароль", max_length=16, required=True, widget=forms.PasswordInput)
     next = forms.CharField(widget=forms.HiddenInput(), required=False)
 
 
